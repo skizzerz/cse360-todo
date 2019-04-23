af8ef49eece26539ea526f3672b47bbc4b9d3218
Adam <aharvey7@asu.edu>
2019-04-22 19:00:23 -0700

    Iterable Changes
    Forgot to include Iterator in previous commit.  Whoops.

--- todo/src/todo/TodoListIterable.java
+++ todo/src/todo/TodoListIterable.java
@@ -4,20 +4,10 @@ import java.util.EnumSet;
 import java.util.Iterator;
 
 public class TodoListIterable implements Iterator<ITodoListItem>, Iterable<ITodoListItem>{
-	private SortBy order;
-	private SortDirection direction;
-	private EnumSet<Status> filter;
 	private TodoListItem head;
 	private TodoListItem next;
 	public TodoListIterable(TodoListItem head){
-		this(SortBy.Priority,SortDirection.Ascending,EnumSet.of(Status.NotStarted,Status.InProgress),head);
-	}
-	public TodoListIterable(SortBy order, SortDirection direction, EnumSet<Status> filter, TodoListItem head) {
-		this.order = order;
-		this.direction = direction;
-		this.filter = filter;
 		this.head = head;
-		this.next = head;
 	}
 	public boolean hasNext() {
 		if(next == null) {
@@ -28,10 +18,7 @@ public class TodoListIterable implements Iterator<ITodoListItem>, Iterable<ITodo
 	}
 	public TodoListItem next() {
 		TodoListItem returnable = next;
-		next = next.traverse(order,direction);
-		while(filter != null && !filter.contains(next.getStatus())) {
-			next = next.traverse(order,direction);
-		}
+		next = next.getNext();
 		return returnable;
 	}
 	public Iterator<ITodoListItem> iterator() {
